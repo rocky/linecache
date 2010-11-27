973e67e589d6085f92d7e6c39e351eee16a37964
Rocky Bernstein <rockyb@rubyforge.org>
2010-11-27 02:49:12 +0000

    Bump version number. We've added static scope since the last release.
    git-svn-id: svn://rubyforge.org/var/svn/rocky-hacks/linecache/branches/rubinius@229 a698f23b-def3-46c9-893a-1fd2e354aa04

--- lib/linecache.rb
+++ lib/linecache.rb
@@ -63,7 +63,7 @@ require_relative 'tracelines'
 # = module LineCache
 # A module to read and cache lines of a Ruby program. 
 module LineCache
-  VERSION = '0.43'
+  VERSION = '0.44.dev'
   LineCacheInfo = Struct.new(:stat, :line_numbers, :lines, :path, :sha1) unless
     defined?(LineCacheInfo)
  
